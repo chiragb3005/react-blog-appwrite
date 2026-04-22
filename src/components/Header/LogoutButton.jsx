e3f88f5ed659e3a234f04ae050dfba17aeeb022d
Chirag Bansal <chiragb3005@gmail.com>
2026-04-23 02:49:13 +0530

    fixed the logout button

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
+import { useNavigate } from "react-router-dom";
 
 function LogoutButton() {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     // have to make the logout handler
     const logoutHandler = () => {
         authService.logout()
         .then(() => {
-            dispatch(logout)
+            dispatch(logout())
+            navigate('/')
         })
     }
 
     return (
         <div>
             <button
+            onClick={logoutHandler}
             className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
             >Logout</button>
         </div>
